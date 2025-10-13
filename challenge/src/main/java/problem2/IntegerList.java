dd73696c35b993a3c2bdf2ee5d6795b8ba41ae14
Mouad Haikal <Mouad.Haikal@um6p.ma>
2025-10-13 12:21:46 +0100

    Problem 2

--- challenge/src/main/java/problem2/IntegerList.java
+++ challenge/src/main/java/problem2/IntegerList.java
@@ -2,20 +2,25 @@ package problem2;
 
 public class IntegerList
 {
-    int[] list; //values in the list
+    private int[] list; //values in the list
+
+    private int integerCount;
+
     //-------------------------------------------------------
 //create a list of the given size
 //-------------------------------------------------------
     public IntegerList(int size)
     {
         list = new int[size];
+
+        integerCount = size;
     }
     //-------------------------------------------------------
 //fill array with integers between 1 and 100, inclusive
 //-------------------------------------------------------
     public void randomize()
     {
-        for (int i=0; i<list.length; i++)
+        for (int i=0; i<integerCount; i++)
             list[i] = (int)(Math.random() * 100) + 1;
     }
     //-------------------------------------------------------
@@ -23,7 +28,49 @@ public class IntegerList
 //-------------------------------------------------------
     public void print()
     {
-        for (int i=0; i<list.length; i++)
+        for (int i=0; i<integerCount; i++)
             System.out.println(i + ":\t" + list[i]);
+    }
+
+// ----------------------------------------------------------
+    private void increaseSize(){
+        int[] newList = new int[2 * list.length];
+
+        for (int i = 0; i < integerCount; i++) {
+            newList[i] = list[i];
+        }
+
+        list = newList;
+    }
+
+    public void addElement(int newVal){
+        if (integerCount >= list.length) increaseSize();
+
+        list[integerCount] = newVal;
+        integerCount++;
+    }
+
+    public boolean removeFirst(int val){
+        int idx=-1;
+
+        for (int i = 0; i < integerCount; i++) {
+            if (list[i] == val) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) return false;
+
+        for (int i = idx+1; i < integerCount; i++) {
+            list[i-1] = list[i];
+        }
+
+        integerCount--;
+        return true;
+    }
+
+    public void removeAll(int val){
+        while (removeFirst(val));
     }
 }
--- challenge/src/main/java/problem2/IntegerListTest.java
+++ challenge/src/main/java/problem2/IntegerListTest.java
@@ -40,6 +40,18 @@ public class IntegerListTest
             case 2:
                 list.print();
                 break;
+            case 3:
+                System.out.print("Enter element to add to the list: ");
+                list.addElement(scan.nextInt());
+                break;
+            case 4:
+                System.out.print("Enter the value to remove: ");
+                list.removeFirst(scan.nextInt());
+                break;
+            case 5:
+                System.out.print("Enter the value to remove: ");
+                list.removeAll(scan.nextInt());
+                break;
             default:
                 System.out.println("Sorry, invalid choice");
         }
@@ -54,6 +66,9 @@ public class IntegerListTest
         System.out.println("0: Quit");
         System.out.println("1: Create a new list (** do this first!! **)");
         System.out.println("2: Print the list");
+        System.out.println("3: Add element");
+        System.out.println("4: Remove a value's first occurence");
+        System.out.println("5: Remove all occurences of a value");
         System.out.print("\nEnter your choice: ");
     }
 }
